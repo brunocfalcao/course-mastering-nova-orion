590461eb45d1ac44b4467da0f4c3314cd175aecc
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2024-02-22 23:38:00 +0100

    wip

--- database/seeders/MasteringNovaOrionCourseSeeder.php
+++ database/seeders/MasteringNovaOrionCourseSeeder.php
@@ -13,6 +13,7 @@ use Eduka\Cube\Models\Video;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class MasteringNovaOrionCourseSeeder extends Seeder
 {
@@ -42,7 +43,7 @@ class MasteringNovaOrionCourseSeeder extends Seeder
             'prelaunched_at' => now()->subDays(30),
             'launched_at' => now()->subDays(15),
 
-            'meta' => [
+            'meta_names' => [
                 'description' => 'my seo description',
                 'author' => 'my seo author',
                 'twitter:site' => 'my seo twitter',
@@ -212,14 +213,97 @@ class MasteringNovaOrionCourseSeeder extends Seeder
                 }
             }
 
+            $descriptions = [
+
+                1 => 'Learn to install Laravel Nova with local repositories or Composer, and optimize with symlinks',
+                2 => 'Explore changes after Nova installation: resources, nova.php config, source files, and API communication',
+                3 => "Understand Nova's default Users Resource: basic properties, modifications, and creating your first custom Resource",
+                4 => 'Create a Products table and Resource in Nova: learn default properties, field creation, and initial configuration',
+                5 => 'Master Nova Fields: configurations, validations, visibility, and extending with custom logic for enhanced functionality',
+
+                6 => 'Create and apply Filters to multiple Nova Resources to manage active statuses, with dynamic header rendering',
+                7 => 'Design custom Lenses in Nova to showcase Top Buyers with sortable computed columns, enhancing data presentation',
+                8 => 'Implement Actions in Nova for bulk status updates on selected Users Resources, with row-specific execution options',
+                9 => 'Explore Metrics in Nova to visualize data with customizable charts across three types for quick insights',
+                10 => 'Dive into Nova Relationships, covering common types and detailing Polymorphic 1-to-Many, saving others for future exploration',
+
+                11 => "Integrate computed attributes into Nova's Global Search by creating custom computed columns for enhanced search results",
+                12 => 'Customize and recompile Nova assets for UI consistency, including adjusting TextArea fields for width alignment',
+                13 => 'Transfer server data to Vue components in Nova, enabling dynamic UI changes like background color adjustments in Text Fields',
+                14 => 'Develop custom UI Components in Nova, such as Fields with server-driven icons, for enriched user interfaces',
+                15 => "Utilize Nova's undocumented 'priority' attribute to strategically sort Resources in the Sidebar for improved navigation",
+
+                16 => 'Implement Abstract Resources in Nova to consolidate common features like Actions and Lenses, and enable default sorting beyond the ID field',
+                17 => "Discover how to configure Nova to load Resources from custom namespaces outside of the default App\Nova",
+                18 => 'Enhance Abstract Resources with dynamic Filters to control column visibility in the index view based on the Resource',
+                19 => "Utilize Nova's Resource Policies for granular authorization, tailoring access and actions within specific data scopes",
+                20 => 'Simplify Nova extension management by consolidating multiple UI components into a single composer package for streamlined development',
+
+                21 => 'Learn to hide specific Resources from the Nova Sidebar while maintaining their usability in Relationships, with optional display authorization',
+                22 => 'Implement dynamic model local scopes in Nova to filter data based on user permissions, avoiding index query filters',
+                23 => "Utilize Model scopes in Nova to restrict visible and interactable data to match the current user's access rights",
+                24 => 'Explore Resource/Model cloning in Nova to categorize data effectively, using a package for cloning with specific scopes',
+                25 => 'Dive into advanced Nova relationships by setting up and utilizing polymorphic relationships (MorphTo) with Resources',
+
+                26 => 'Master complex relationships in Nova with access to pivot table fields and custom pivot models for enriched data associations',
+                27 => 'Customize Nova by exporting and modifying default stub files for tailored Resource generation and functionality',
+                28 => 'Utilize field behavior customization in Nova 3.1.0 to alter field data presentation across different contexts: index, form, and detail',
+                29 => 'Develop a comprehensive Resource checklist in Nova to ensure thorough coverage of actions and validations for Resource creation',
+                30 => "Understand the structure of Nova's UI components, including Fields, Cards, Lenses, Filters, Metrics, and Tools, for custom UI integration",
+
+                31 => 'Enhance a Nova Tool to exchange data between the frontend and server, demonstrating data transformation and return to the frontend',
+                32 => 'Explore Vue properties provided by Nova to leverage in custom tools and extensions for optimal functionality',
+                33 => "Integrate existing Nova Vue Components into your custom Tool to utilize Nova's built-in UI elements and avoid redundancy",
+                34 => "Utilize Nova's event broadcasting with \$emit to facilitate communication between frontend UI components for dynamic interactions",
+                35 => "Demonstrate \$emit in action with a practical example of interlinked dropdowns, where one dropdown's selection influences another",
+                36 => 'Begin developing a ticketing system by setting up the foundational composer package for the Nova Tool',
+                37 => "Advance the Tool's development with proper authorization and operator permissions, emphasizing the importance of context in permission handling",
+                38 => 'Implement a quick filter for Operators in the ticketing system to sort tickets by assignment status, enhancing usability',
+                39 => 'Create an Action for Operators within the ticketing system to self-assign tickets, showcasing user-specific functionality',
+                40 => 'Develop a Supervisor-exclusive Action in the ticketing system to assign tickets to selectable Operators, demonstrating role-based capabilities',
+
+                41 => "Optimize the 'Assign to Myself' action to display only for tickets without an assigned Operator, streamlining the ticketing system",
+                42 => 'Introduce a new action to requeue tickets, showcasing a unique Nova behavior for managing ticket states effectively',
+                43 => "Develop a 'Total Tickets' KPI card from scratch for a custom Nova Dashboard, enhancing data visualization",
+                44 => "Test Nova Resources efficiently with Brian Dillingham's package for a quick and insightful initial review",
+                45 => 'Enable Index View search functionality in relationship columns in Nova with a specialized package for enhanced usability',
+                46 => "Incorporate Brian Dillingham's versatile button field in Nova, allowing for customizable actions within Resources",
+                47 => 'Link multiple dropdowns in Nova to dynamically update child dropdowns based on parent selections, improving UI interactivity',
+                48 => 'Utilize a Nova package for advanced data visualization on dashboards, elevating the presentation of key metrics',
+                49 => "Enhance Nova's UI with a responsiveness package, adapting the admin panel for better mobile and tablet usability",
+                50 => "Integrate OAuth authentication in Nova's login workflow with third-party providers like GitHub or Twitter for enhanced security and user experience",
+
+                51 => "Implement Spatie's multitenancy package in Nova to manage database connections dynamically based on tenant subdomains, streamlining multi-tenant applications",
+            ];
+
             foreach ($videos as $oldVideo) {
+
+                /**
+<meta name="twitter:site" content="@tailwindcss" inertia>
+<meta name="twitter:title" content="Tailwind UI - Official Tailwind CSS Components & Templates" inertia>
+<meta name="twitter:description" content="Beautiful UI components and templates by the creators of Tailwind CSS." inertia>
+<meta name="twitter:image" content="https://tailwindui.com/img/og-default.png" inertia>
+<meta name="twitter:creator" content="@tailwindcss" inertia>
+<meta property="og:url" content="https://www.tailwindui.com/" inertia>
+<meta property="og:type" content="article" inertia>
+<meta property="og:title" content="Tailwind UI - Official Tailwind CSS Components & Templates" inertia>
+<meta property="og:description" content="Beautiful UI components and templates by the creators of Tailwind CSS." inertia>
+<meta property="og:image" content="https://tailwindui.com/img/og-default.png" inertia>
+<meta property="description" content="Beautiful UI components and templates by the creators of Tailwind CSS." inertia>
+                 */
+                $uuid = (string) Str::uuid();
+
                 $newVideo = Video::create([
                     'old_id' => $oldVideo->id,
                     'name' => $oldVideo->title,
+                    'uuid' => $uuid,
                     'description' => $oldVideo->details,
                     'chapter_id' => $newChapter->id,
                     'course_id' => $course->id,
                     'duration' => $oldVideo->duration,
+                    'is_visible' => $oldVideo->is_visible,
+                    'is_active' => $oldVideo->is_active,
+                    'is_free' => $oldVideo->is_free,
                 ]);
 
                 // Attach video image for SEO.
@@ -234,17 +318,45 @@ class MasteringNovaOrionCourseSeeder extends Seeder
                 }
             }
 
-            $completedList = clone $oldVideosCompleted;
+            // Update the SEO metadata for each video.
+            foreach (Video::all() as $video) {
+                if ($video->old_id) {
+                    $video->update([
+                        'meta_names' => [
+                            'twitter:site' => env('MN_OR_DOMAIN').'/video/'.$video->uuid,
+                            'twitter:title' => $video->name,
+                            'twitter:description' => $descriptions[$video->old_id],
+                            'twitter:image' => env('MN_OR_DOMAIN').'/storage/'.$video->filename,
+                            'twitter:creator' => '@'.env('MN_OR_TWITTER'),
+                            '',
+                        ],
+                        'meta_properties' => [
+                            'twitter:site' => env('MN_OR_DOMAIN').'/video/'.$video->uuid,
+                            'twitter:title' => $video->name,
+                            'twitter:description' => $descriptions[$video->old_id],
+                            'twitter:image' => env('MN_OR_DOMAIN').'/storage/'.$video->filename,
+                            'twitter:creator' => '@'.env('MN_OR_TWITTER'),
+                        ],
+                    ]);
+                }
+            }
 
-            foreach (clone $completedList->get() as $videoCompleted) {
+            foreach (clone $oldVideosCompleted->get() as $videoCompleted) {
                 $user = User::firstWhere('id', $videoCompleted->user_id);
                 $video = Video::firstWhere('old_id', $videoCompleted->video_id);
 
                 if ($video && $user) {
-                    $video->usersThatSaw()->attach(
-                        $user,
-                        ['created_at' => $videoCompleted->created_at]
-                    );
+                    // Delete other lines
+                    DB::table('user_video_seen')->where([
+                        'user_id' => $user->id,
+                        'video_id' => $video->id,
+                    ])->delete();
+
+                    DB::table('user_video_seen')->insert([
+                        'user_id' => $user->id,
+                        'video_id' => $video->id,
+                        'created_at' => $videoCompleted->created_at,
+                    ]);
                 }
             }
         }
@@ -277,6 +389,12 @@ class MasteringNovaOrionCourseSeeder extends Seeder
                         'created_at' => $paddleLog->created_at,
                         'updated_at' => $paddleLog->created_at,
                     ]);
+
+                    // Attach user to Orion course.
+                    $user->courses()->attach($variant->course);
+
+                    // Attach user to course variant.
+                    $user->variants()->attach($variant);
                 }
             });
         }
